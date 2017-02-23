d4d5cd4d952705bf5ed0d9bdb548a57ebf5d65fa
Jonathan Pinlac <jonppinlac@gmail.com>
2017-02-23 13:44:32 -0500

    Reorginze project and added redux and react-toolbox

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './Containers/App';
 import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+// import reducer form './Reducers/reducer';
+
+const store = createStore(function(state = { test: "Test" }, action) {
+    return state;
+});
+store.subscribe( () => {
+    console.log(JSON.stringify(store.getState(), null, " "));
+});
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+      <App />
+  </Provider>,
   document.getElementById('root')
 );
